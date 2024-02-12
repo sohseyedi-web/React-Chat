1604f7dbc711476318ce8a085701e8d9bfcf26dc
sohseyedi <sohseyedi@gmail.com>
2024-02-12 11:02:33 +0330

    message

---
+++ src/features/home/message/Message.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Message = ({item}) => {
+  return (
+    <div>Message</div>
+  )
+}
+
+export default Message
--- src/features/home/message/MessageContainer.jsx
+++ src/features/home/message/MessageContainer.jsx
@@ -1,15 +1,16 @@
 import * as RiIcon from "react-icons/ri";
 import { useSocketContext } from "../../../context/SocketProvider";
 import { useGetMessageById } from "../../../hooks/useMessage";
+import Loading from "../../../ui/Loading";
+import Message from "./Message";
 import MessageInput from "./MessageInput";
 
 const MessageContainer = ({ user }) => {
-
-  const {data,isLoading} = useGetMessageById(user?._id);
-  const {onlineUsers} = useSocketContext();
+  const { data, isLoading } = useGetMessageById(user?._id);
+  const { onlineUsers } = useSocketContext();
   const isOnline = onlineUsers.includes(user?._id);
 
-  console.log(isOnline)
+  if (isLoading) return <Loading />;
 
   return (
     <section className="flex flex-col min-h-screen">
@@ -17,13 +18,23 @@ const MessageContainer = ({ user }) => {
         <div className="flex items-center gap-x-2">
           <img src={user?.profilePic} alt={user?.name} className="w-10 h-10" />
           <h6 className="text-xl font-semibold">{user?.username}</h6>
-          {isOnline ? <span className="w-3 h-3 animate-pulse rounded-full bg-green-500"></span> : null}
+          {isOnline ? (
+            <span className="w-3 h-3 animate-pulse rounded-full bg-green-500"></span>
+          ) : null}
         </div>
         <RiIcon.RiSearchLine className=" text-gray-900" size={26} />
       </header>
-      <div className="flex-1">sss</div>
+      <div className="flex-1">
+        {!data?.length ? (
+          <div className="flex h-screen justify-center items-center px-3 rounded-md bg-gray-400 text-gray-950 shadow py-1">
+            هیچ پیامی وجود ندارد
+          </div>
+        ) : (
+          data?.map((item) => <Message item={item} key={item?._id} />)
+        )}
+      </div>
       <footer className="flex w-full items-center justify-between py-2 px-2 border-t">
-        <MessageInput user={user}/>
+        <MessageInput user={user} />
       </footer>
     </section>
   );
