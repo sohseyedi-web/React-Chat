7716af1e38922632b43a87f8dd5a41bb2a53719a
sohseyedi <sohseyedi@gmail.com>
2024-02-17 17:28:07 +0330

    bug fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Auth from "./pages/Auth";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
-import ProtectedRoutes from './ui/ProtectedRoutes';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <ProtectedRoutes>
-              <Home />
-            </ProtectedRoutes>
-          }
-        />
+        <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Auth />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useEffect } from "react";
 import * as RiIcon from "react-icons/ri";
+import { useNavigate } from "react-router-dom";
 import MessageLayout from "../features/home/message/MessageLayout";
 import Sidebar from "../features/home/sidebar/Sidebar";
+import { useGetUser } from "../hooks/useUser";
 import useHandleUsers from "../zustand/useHandleUsers";
 
 const Home = () => {
   const { isActive, setIsActive } = useHandleUsers();
+  const { user, isLoading } = useGetUser();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) return navigate("/auth");
+  }, [user, isLoading, navigate]);
 
   return (
     <section className="flex mx-auto w-full h-screen relative overflow-hidden">
